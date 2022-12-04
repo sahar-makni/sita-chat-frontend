79c07bbae283c1b8f67f84309fcff1a9d93d95f8
sahar makni <sahar.makni@squeezer-software.com>
2022-12-04 23:40:27 +0100

    add first test

--- src/app/sign-in/sign-in.component.spec.ts
+++ src/app/sign-in/sign-in.component.spec.ts
@@ -1,16 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SignInComponent } from './sign-in.component';
+import {SignInComponent} from './sign-in.component';
 
-describe('SignInComponent', () => {
+import {Pipe, PipeTransform} from '@angular/core';
+
+
+@Pipe({name: 'translate'})
+class TranslationPipeMock implements PipeTransform {
+  transform(value: any, ...args: any[]): any {
+    return value;
+  }
+}
+
+describe(SignInComponent.name, () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignInComponent ]
-    })
-    .compileComponents();
+      declarations: [SignInComponent, TranslationPipeMock]
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +30,13 @@ describe('SignInComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show form with email and password', () => {
+    const inputs = fixture.nativeElement.querySelectorAll('input');
+    expect(inputs).toBeTruthy();
+    expect(inputs.length).toBe(2);
+    expect(inputs[0].type).toEqual('email');
+    expect(inputs[1].type).toEqual('password');
   });
 });
