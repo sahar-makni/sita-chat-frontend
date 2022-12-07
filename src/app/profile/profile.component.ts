85134d9f52648d6d8827043cf28cd5edd0d2138f
sahar makni <sahar.makni@squeezer-software.com>
2022-12-07 17:16:50 +0100

    change language

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {ThemeOption, ThemeService} from '../common/theme.service';
 
+export type Languages = 'EN' | 'FR';
 
 
 @Component({
@@ -10,23 +11,40 @@ import {ThemeOption, ThemeService} from '../common/theme.service';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  themes = [
-    {label: this.translateService.instant('profile.theme.dark'), value: 'DARK'},
-    {label: this.translateService.instant('profile.theme.light'), value: 'LIGHT'}
-  ];
 
+  themes: { label: string, value: string }[];
+  languages: { label: string, value: string }[];
   selectedTheme: ThemeOption = 'DARK';
+  selectedLanguage: Languages = 'EN';
 
   constructor(private readonly translateService: TranslateService,
               private readonly themeService: ThemeService,
-              ) {
+  ) {
   }
 
   ngOnInit(): void {
+    this.setUpOptions();
   }
 
-  handleSelectedTheme(newTheme: ThemeOption): void {
-    this.selectedTheme = newTheme;
-    this.themeService.switchTheme(newTheme);
+  setUpOptions(): void {
+    this.themes = [
+      {label: this.translateService.instant('profile.theme.dark'), value: 'DARK'},
+      {label: this.translateService.instant('profile.theme.light'), value: 'LIGHT'}
+    ];
+    this.languages = [
+      {label: this.translateService.instant('profile.language.en'), value: 'EN'},
+      {label: this.translateService.instant('profile.language.fr'), value: 'FR'}
+    ];
+  }
+
+  handleSelectedTheme(theme: ThemeOption): void {
+    this.selectedTheme = theme;
+    this.themeService.switchTheme(theme);
+  }
+
+  handleSelectedLanguage(language: Languages): void {
+    console.log(language);
+    this.selectedLanguage = language;
+    this.translateService.use(language.toLowerCase());
   }
 }
