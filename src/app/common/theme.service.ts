887b4e459905b2990320d6bd9306eb8b93dd25c7
sahar makni <sahar.makni@squeezer-software.com>
2022-12-08 12:42:05 +0100

    keep theme on refresh

--- src/app/common/theme.service.ts
+++ src/app/common/theme.service.ts
@@ -1,17 +1,30 @@
-import { Inject, Injectable } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import {Inject, Injectable} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
+import {WEB_LOCAL_STORAGE} from "../utils/providers/web-storage.provider";
+import {USER_THEME} from "../utils/const/general";
+
 export type ThemeOption = 'ARYA_BLUE' | 'SAGA_BLUE';
+
 @Injectable({
   providedIn: 'root',
 })
 export class ThemeService {
-THEME_MAP = {
-  ARYA_BLUE: 'arya-blue',
-  SAGA_BLUE: 'saga-blue',
-};
-  constructor(@Inject(DOCUMENT) private document: Document) {}
+  THEME_MAP = {
+    ARYA_BLUE: 'arya-blue',
+    SAGA_BLUE: 'saga-blue',
+  };
 
-  switchTheme(theme: ThemeOption): void {
+  constructor(@Inject(DOCUMENT) private document: Document,
+              @Inject(WEB_LOCAL_STORAGE) private readonly localStorage: Storage,
+  ) {
+    this.switchTheme(this.localStorage.getItem(USER_THEME) as ThemeOption);
+  }
+
+
+  switchTheme(theme: ThemeOption | undefined): void {
+    if (!theme) {
+      return;
+    }
     const themeLink = this.document.getElementById('app-theme') as HTMLLinkElement;
 
     if (themeLink) {
