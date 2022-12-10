e7104290ddb3045d5bd2ef6e77bf5fa81edf4afe
sahar makni <sahar.makni@squeezer-software.com>
2022-12-10 13:17:48 +0100

    Poll WS get room messages Not the best solution (I know)

--- src/app/chat/chat-room.service.ts
+++ src/app/chat/chat-room.service.ts
@@ -1,9 +1,10 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {interval, Observable} from 'rxjs';
 import {ChatRoomResponse, MessageResponse} from './chat-room.type';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {MESSAGES_WS_PATH, ROOM_WS_PATH} from '../utils/const/general';
+import {switchMap} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class ChatRoomService {
@@ -21,7 +22,12 @@ export class ChatRoomService {
 
   getRoomMessages(roomId: number): Observable<MessageResponse[]> {
     const url = `${environment.baseUrl}${ROOM_WS_PATH}/${roomId}${MESSAGES_WS_PATH}`;
-    return this.httpClient.get<MessageResponse[]>(url);
+    return interval(1000).pipe(
+      switchMap(() => {
+        return this.httpClient.get<MessageResponse[]>(url);
+      })
+    );
+
   }
 
   sendMessage(roomId: number, newMessage: string): Observable<void> {
