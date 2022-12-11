173e42544ec89743ba4ae027d6f8af0a6d2725b6
sahar makni <sahar.makni@squeezer-software.com>
2022-12-11 01:04:11 +0100

    UT sign-in.component.ts

--- src/app/sign-in/sign-in.component.spec.ts
+++ src/app/sign-in/sign-in.component.spec.ts
@@ -5,7 +5,7 @@ import {SignInComponent} from './sign-in.component';
 import {Pipe, PipeTransform} from '@angular/core';
 import {RouterTestingModule} from '@angular/router/testing';
 import {TranslateService} from '@ngx-translate/core';
-import {FormBuilder} from '@angular/forms';
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
 import {HttpClient} from '@angular/common/http';
 import {UserService} from '../common/user.service';
 
@@ -17,6 +17,8 @@ class TranslationPipeMock implements PipeTransform {
   }
 }
 
+// these are few tests that I would have written more if I had more time
+
 describe(SignInComponent.name, () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
@@ -24,8 +26,10 @@ describe(SignInComponent.name, () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SignInComponent, TranslationPipeMock],
-      imports: [ RouterTestingModule],
-      providers: [{provide: TranslateService , useValue: {}}, FormBuilder,
+      imports: [RouterTestingModule, ReactiveFormsModule],
+      providers: [
+        {provide: TranslateService, useValue: {}},
+        FormBuilder,
         {provide: HttpClient, useValue: {}},
         {provide: UserService, useValue: {}},
       ],
@@ -48,5 +52,134 @@ describe(SignInComponent.name, () => {
     expect(inputs.length).toBe(2);
     expect(inputs[0].type).toEqual('email');
     expect(inputs[1].type).toEqual('password');
+  });
+  describe('email', () => {
+    it('should accept valid email', async () => {
+      // GIVEN
+      const emailInput = fixture.nativeElement.querySelector('#email') as HTMLInputElement;
+      fixture.detectChanges();
+      emailInput.value = 'sahar.makni@sita.com';
+      emailInput.dispatchEvent(new Event('input'));
+      component.form.controls.email.markAsTouched();
+      // WHEN
+      fixture.detectChanges();
+      // THEN
+      const wrongFormatErrorMessage = fixture.nativeElement.querySelector('[data-test-email-format-message]') as HTMLSpanElement;
+      const emptyErrorMessage = fixture.nativeElement.querySelector('[data-test-email-empty-message]') as HTMLSpanElement;
+      expect(wrongFormatErrorMessage).toBeFalsy();
+      expect(emptyErrorMessage).toBeFalsy();
+    });
+    it('should show invalid message for invalid email', async () => {
+      // GIVEN
+      const emailInput = fixture.nativeElement.querySelector('#email') as HTMLInputElement;
+      fixture.detectChanges();
+      emailInput.value = 'not an email';
+      emailInput.dispatchEvent(new Event('input'));
+      component.form.controls.email.markAsTouched();
+      // WHEN
+      fixture.detectChanges();
+      // THEN
+      const wrongFormatErrorMessage = fixture.nativeElement.querySelector('[data-test-email-format-message]') as HTMLSpanElement;
+      expect(wrongFormatErrorMessage).toBeTruthy();
+      expect(wrongFormatErrorMessage.textContent).toContain('connexion.errors.invalidEmailFormat');
+      const emptyErrorMessage = fixture.nativeElement.querySelector('[data-test-email-empty-message]') as HTMLSpanElement;
+      expect(emptyErrorMessage).toBeFalsy();
+
+    });
+    it('should show required message for empty email', async () => {
+      // GIVEN
+      const emailInput = fixture.nativeElement.querySelector('#email') as HTMLInputElement;
+      fixture.detectChanges();
+      emailInput.value = '';
+      emailInput.dispatchEvent(new Event('input'));
+      component.form.controls.email.markAsTouched();
+      // WHEN
+      fixture.detectChanges();
+      // THEN
+      const wrongFormatErrorMessage = fixture.nativeElement.querySelector('[data-test-email-format-message]') as HTMLSpanElement;
+      expect(wrongFormatErrorMessage).toBeFalsy();
+      const emptyErrorMessage = fixture.nativeElement.querySelector('[data-test-email-empty-message]') as HTMLSpanElement;
+      expect(emptyErrorMessage).toBeTruthy();
+      expect(emptyErrorMessage.textContent).toContain('connexion.errors.emailRequired');
+
+    });
+  });
+  describe('password', () => {
+    it('should accept valid password', async () => {
+      // GIVEN
+      const passwordInput = fixture.nativeElement.querySelector('#password') as HTMLInputElement;
+      fixture.detectChanges();
+      passwordInput.value = '51MP13 4ND 10N6 P455W0rD';
+      passwordInput.dispatchEvent(new Event('input'));
+      component.form.controls.password.markAsTouched();
+      // WHEN
+      fixture.detectChanges();
+      // THEN
+
+      const emptyErrorMessage = fixture.nativeElement.querySelector('[data-test-password-empty-message]') as HTMLSpanElement;
+      expect(emptyErrorMessage).toBeFalsy();
+    });
+    it('should show required message for empty password', async () => {
+      // GIVEN
+      const passwordInput = fixture.nativeElement.querySelector('#password') as HTMLInputElement;
+      fixture.detectChanges();
+      passwordInput.value = '';
+      passwordInput.dispatchEvent(new Event('input'));
+      component.form.controls.password.markAsTouched();
+      // WHEN
+      fixture.detectChanges();
+      // THEN
+      const emptyErrorMessage = fixture.nativeElement.querySelector('[data-test-password-empty-message]') as HTMLSpanElement;
+      expect(emptyErrorMessage).toBeTruthy();
+      expect(emptyErrorMessage.textContent).toContain('connexion.errors.passwordRequired');
+
+    });
+  });
+  describe('sign in button', () => {
+
+    it('should be disabled if form is invalid', () => {
+      // GIVEN
+      const emailInput = fixture.nativeElement.querySelector('#email') as HTMLInputElement;
+      const passwordInput = fixture.nativeElement.querySelector('#password') as HTMLInputElement;
+
+      fixture.detectChanges();
+
+      emailInput.value = 'not an email';
+      passwordInput.value = '';
+
+      emailInput.dispatchEvent(new Event('input'));
+      passwordInput.dispatchEvent(new Event('input'));
+
+      component.form.controls.email.markAsTouched();
+      component.form.controls.password.markAsTouched();
+      // WHEN
+      fixture.detectChanges();
+      // THEN
+      const signInButton = fixture.nativeElement.querySelector('button[type=button]') as HTMLButtonElement;
+      expect(signInButton).toBeTruthy();
+      expect(signInButton.disabled).toBeTrue();
+    });
+    it('should be enabled if form is valid', () => {
+      // GIVEN
+      const emailInput = fixture.nativeElement.querySelector('#email') as HTMLInputElement;
+      const passwordInput = fixture.nativeElement.querySelector('#password') as HTMLInputElement;
+
+      fixture.detectChanges();
+
+      emailInput.value = 'email@example.com';
+      passwordInput.value = 'password1!';
+
+      emailInput.dispatchEvent(new Event('input'));
+      passwordInput.dispatchEvent(new Event('input'));
+
+      component.form.controls.email.markAsTouched();
+      component.form.controls.password.markAsTouched();
+      // WHEN
+      fixture.detectChanges();
+      // THEN
+      const signInButton = fixture.nativeElement.querySelector('button[type=button]') as HTMLButtonElement;
+      expect(signInButton).toBeTruthy();
+      expect(signInButton.disabled).toBeFalse();
+    });
   });
 });
